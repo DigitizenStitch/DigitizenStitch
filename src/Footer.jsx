826277c3ffce650b29967c93c37f193506d412ec
nabeel-78 <133483809+nabeel-78@users.noreply.github.com>
2025-03-19 03:24:31 +0500

    Updated Footer.jsx with correct logo import

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChevronRight, ChevronUp } from 'lucide-react';
-import LogoImage from "../assets/logo.png"; 
+import LogoImage from "/src/assets/Logo.png"; 
 import { FaInstagram, FaFacebookF, FaTwitter, FaWhatsapp } from 'react-icons/fa';
 
 export function FooterTwo() {
